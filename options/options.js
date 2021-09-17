fc536d5bd307f258063502300dcf127684480662
Crowdbotics <team@crowdbotics.com>
2021-09-17 20:38:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "white_tooth_30652Identifier",
+  url: "https://white_tooth_30652Identifier.botics.co",
+  api: "https://white_tooth_30652Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
